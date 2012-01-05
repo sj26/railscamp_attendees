09f387f2a959c6028c853e96a69e791122c62736
Sebastian von Conrad <sebastian@e-channel.com.au>
2012-01-05 19:20:04 +1030

    Remove "Name" and "Handle" from headers

--- normalize.rb
+++ normalize.rb
@@ -3,10 +3,10 @@ require 'csv'
 attendees = CSV.read('rcx.csv', headers: true)
 
 CSV.open('rcx.csv', 'wb') do |csv|
-  csv << ['First Name', 'Last Name', 'Twitter Handle']
+  csv << ['First', 'Last', 'Twitter']
 
-  attendees.sort { |x, y| x['First Name'] <=> y['First Name'] }.each do |attendee|
-    attendee['Twitter Handle'].gsub!(/^@?/, '@') if attendee['Twitter Handle']
+  attendees.sort { |x, y| x['First'] <=> y['First'] }.each do |attendee|
+    attendee['Twitter'].gsub!(/^@?/, '@') if attendee['Twitter']
     csv << attendee.fields.map { |f| f.strip if f.respond_to?(:strip) }
   end
 end
