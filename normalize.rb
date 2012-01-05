00dd856ad904beb8f0e1d85234725380c63eb57e
Sebastian von Conrad <sebastian@e-channel.com.au>
2012-01-05 17:22:46 +1030

    Order by first name

--- normalize.rb
+++ normalize.rb
@@ -5,7 +5,7 @@ attendees = CSV.read('rcx.csv', headers: true)
 CSV.open('rcx.csv', 'wb') do |csv|
   csv << ['First Name', 'Last Name', 'Twitter Handle']
 
-  attendees.each do |attendee|
+  attendees.sort { |x, y| x['First Name'] <=> y['First Name'] }.each do |attendee|
     attendee['Twitter Handle'].gsub!(/^@?/, '@') if attendee['Twitter Handle']
     csv << attendee.fields.map { |f| f.strip if f.respond_to?(:strip) }
   end
