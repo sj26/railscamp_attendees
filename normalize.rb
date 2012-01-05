95b4e2a64bdbf76c9565349b3c278a0daa5f8712
Sebastian von Conrad <sebastian@e-channel.com.au>
2012-01-05 19:26:29 +1030

    Refactor sorting

--- normalize.rb
+++ normalize.rb
@@ -1,11 +1,11 @@
 require 'csv'
 
-attendees = CSV.read('rcx.csv', headers: true)
+attendees = CSV.read('rcx.csv', headers: true).sort { |x, y| x['First'] <=> y['First'] }
 
 CSV.open('rcx.csv', 'wb') do |csv|
   csv << ['First', 'Last', 'Twitter']
 
-  attendees.sort { |x, y| x['First'] <=> y['First'] }.each do |attendee|
+  attendees.each do |attendee|
     attendee['Twitter'].gsub!(/^@?/, '@') if attendee['Twitter']
     csv << attendee.fields.map { |f| f.strip if f.respond_to?(:strip) }
   end
